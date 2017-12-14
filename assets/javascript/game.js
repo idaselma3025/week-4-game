351f2028919868cb559e7028f603612d2b601ad7
idaselma3025 <idaselma3025@gmail.com>
2017-12-13 21:29:01 -0600

    better

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,67 @@
 $(document).ready(function() {
   //VARIABLES
   var random;
+  var score;
+  var hidden;
+  var poop;
+  var winCounter=0;
+  var lossCounter=0;
 
-  //function to create and print random number on restart of game
-  function getRandomNumber(min,max){
+function setCurrent (){
+  $("#currentScore").attr("data-current",0);
+  poop = ($("#currentScore").attr("data-current"));
+  poop = parseInt(poop);
+}
+  // function to create and print random number on restart of game
+  function getRandomNumber(){
     random = Math.floor(Math.random() *(120-18)) + 18;
     $("#randomNumber").text(random);
+    $("img").each(function(){
+      hidden = Math.floor(Math.random() * (12)) + 1;
+      $(this).attr("data-hidden",hidden);
+      console.log(hidden);
+    });
   }
   getRandomNumber();
+  setCurrent();
 
   //function to assign hidden gem values between 1 and 12
+  // $("img").each(function(){
+  //   hidden = Math.floor(Math.random() * (12)) + 1;
+  //   $(this).attr("data-hidden",hidden);
+  //   console.log(hidden);
+  // });
 
-  $("input").each(function(){
-    var hidden = Math.floor(Math.random() * (11)) + 1;
-    $(this).attr("data-hidden",hidden);
-    console.log($(this).attr("data-hidden"));
-    console.log(hidden);
+  $("img").on("click",function(){
+    if (poop === 0) {
+    var crap = ($(this).attr("data-hidden"));
+    crap = parseInt(crap);
+    $("#currentScore").attr("data-current",crap);
+    $("#currentScore").text(crap);
+    poop = crap;
+}
+else{
+  var crap = ($(this).attr("data-hidden"));
+  crap = parseInt(crap);
+  poop = crap + poop;
+  $("#currentScore").text(poop);
+}
+if(poop === random){
+  alert("good job");
+  getRandomNumber();
+  $("#currentScore").empty();
+  setCurrent();
+  winCounter ++;
+  $("#wins").text(winCounter);
+}
+else if(poop>random){
+  alert("you suck");
+  getRandomNumber();
+  $("#currentScore").empty();
+  setCurrent();
+  lossCounter++;
+  $("#losses").text(lossCounter);
+
+}
   });
 });
