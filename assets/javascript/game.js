a63a204de95284d7ff4529bfa87da162ced5dc33
idaselma3025 <idaselma3025@gmail.com>
2017-12-29 09:46:19 -0600

    fixed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,64 +1,96 @@
-$(document).ready(function() {
-  //VARIABLES
-  var random;
-  var score;
-  var hidden;
-  var poop;
-  var winCounter=0;
-  var lossCounter=0;
+//GLOBAL VARIABLES
+//---------------------------------------------------
 
-function setCurrent (){
-  $("#currentScore").attr("data-current",0);
-  poop = ($("#currentScore").attr("data-current"));
-  poop = parseInt(poop);
+//Crystal Object
+var crystal = {
+  blue:{
+    name:"blue",
+    value:0
+  },
+  green:{
+    name:"green",
+    value:0
+  },
+  red:{
+    name:"red",
+    value:0
+  },
+  yellow:{
+    name:"yellow",
+    value:0
+  },
+};
+
+//Scores (current and target)
+var targetScore   = 0;
+var currentScore  = 0;
+
+//Wins and Losses
+var winCount      = 0;
+var lossCount     = 0;
+
+//FUNCTIONS
+//---------------------------------------------------
+
+//generate a random number FUNCTION
+function randomNumber(min,max){
+  return Math.floor(Math.random()*(max-min + 1))+min;
 }
-  // function to create and print random number on restart of game
-  function getRandomNumber(){
-    random = Math.floor(Math.random() *(120-18)) + 18;
-    $("#randomNumber").text(random);
-    $("img").each(function(){
-      hidden = Math.floor(Math.random() * (12)) + 1;
-      $(this).attr("data-hidden",hidden);
-      console.log(hidden);
-    });
+//start game FUNCTION
+function startGame(){
+  //set a new targetScore to a random number 19-120
+  targetScore = randomNumber(19,120);
+  //set each gem to random number 1-12
+  crystal.blue.value    = randomNumber(1,12);
+  crystal.green.value   = randomNumber(1,12);
+  crystal.yellow.value  = randomNumber(1,12);
+  crystal.red.value     = randomNumber(1,12);
+  //reset the currentScore to 0
+  currentScore = 0;
+  //change the html
+  $("#currentScore").html(currentScore);
+  $("#randomNumber").html(targetScore);
+  //console logging
+  console.log("targetScore: "+targetScore);
+  console.log(" blue: "+ crystal.blue.value + " yellow: " + crystal.yellow.value + " red :"+ crystal.red.value + " green: "+ crystal.green.value);
+
+}
+//function to add crystal values
+function crystalAdd(crystal){
+  currentScore = currentScore + crystal.value;
+  console.log("yourscore: " + currentScore);
+  $("#currentScore").html(currentScore);
+  checkWin();
+}
+
+//function check wins
+function checkWin(){
+  if (currentScore > targetScore){
+    alert("you lost");
+    ++lossCount
+    $("#losses").html(lossCount);
+    startGame();
   }
-  getRandomNumber();
-  setCurrent();
+  else if (currentScore == targetScore){
+    alert("you won");
+    ++winCount
+    $("#wins").html(winCount);
+    startGame();
+  }
+}
+//MAIN PROCESS
+//---------------------------------------------------
+startGame();
 
-  $("img").on("click",function(){
-    if (poop === 0) {
-    var crap = ($(this).attr("data-hidden"));
-    crap = parseInt(crap);
-    $("#currentScore").attr("data-current",crap);
-    $("#currentScore").text(crap);
-    poop = crap;
-}
-else{
-  var crap = ($(this).attr("data-hidden"));
-  crap = parseInt(crap);
-  poop = crap + poop;
-  $("#currentScore").text(poop);
-}
-getWins();
+$("#gemBlue").click(function(){
+  crystalAdd(crystal.blue);
 });
-function getWins(){
-if(poop === random){
-  alert("good job");
-  getRandomNumber();
-  $("#currentScore").empty();
-  setCurrent();
-  winCounter ++;
-  $("#wins").text(winCounter);
-}
-else if(poop>random){
-  alert("you suck");
-  getRandomNumber();
-  $("#currentScore").empty();
-  setCurrent();
-  lossCounter++;
-  $("#losses").text(lossCounter);
-
-}
-}
-
+$("#gemYellow").click(function(){
+  crystalAdd(crystal.yellow);
+});
+$("#gemGreen").click(function(){
+  crystalAdd(crystal.green);
+});
+$("#gemRed").click(function(){
+  crystalAdd(crystal.red);
 });
